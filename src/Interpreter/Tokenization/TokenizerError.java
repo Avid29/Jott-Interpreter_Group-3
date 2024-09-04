f73296e0d5cab748f3c1cd9ad31a119ee32f9c72
Adam Dernis <Avid29@Live.com>
2024-09-04 00:43:37 -0400

    Implemented situation specifc tokenization error messages

--- src/Interpreter/Tokenization/TokenizerError.java
+++ src/Interpreter/Tokenization/TokenizerError.java
@@ -8,7 +8,8 @@ public class TokenizerError {
     private String tokenProgress;
     private char nextChar;
 
-    public TokenizerError(String filename, int lineNum, int columnNum, TokenizerState state, String tokenProgress, char nextChar) {
+    public TokenizerError(String filename, int lineNum, int columnNum, TokenizerState state, String tokenProgress,
+            char nextChar) {
         this.filename = filename;
         this.lineNum = lineNum;
         this.columnNum = columnNum;
@@ -20,8 +21,32 @@ public class TokenizerError {
     @Override
     public String toString() {
         String header = "Syntax Error\n";
-        String message = String.format("'%c' is not a valid character in %s state", nextChar, state.name());
+        String message = errorAsMessage();
         String footer = String.format("%s:%d%n", filename, lineNum, columnNum);
         return header + message + "\n" + footer;
+    }
+
+    private String errorAsMessage() {
+        if (state == TokenizerState.BANG) {
+            // Failed due to lone '!'
+            return "Invalid token \"!\". Expected '='.";
+        }
+
+        if (state == TokenizerState.DECIMAL) {
+            // Failed due to lone '.'
+            return "Invalid token \".\". Expected leading or trailing digit";
+        }
+
+        if (state == TokenizerState.STRING) {
+            // Failed due to unclosed string
+            return "String literal missing closing quotes";
+        }
+
+        if (nextChar == '\0') {
+            // Failed in finalization
+            return String.format("\"%s\" is not a complete token.", tokenProgress);
+        }
+
+        return "Unrecognizable error";
     }
 }
