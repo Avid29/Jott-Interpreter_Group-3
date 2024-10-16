2e54a577cb8a3836d602a3354c246fd811764d55
amenkes319 <amenkes319@gmail.com>
2024-10-16 16:21:13 -0400

    BodyNode parseNode implemented

--- Interpreter/ProgramTree/Nodes/BodyNode.java
+++ Interpreter/ProgramTree/Nodes/BodyNode.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import Interpreter.Parsing.TokenStack;
 import Interpreter.ProgramTree.Nodes.Abstract.NodeBase;
 import Interpreter.ProgramTree.Nodes.StatementNodes.Abstract.BodyStatementNodeBase;
+import provided.TokenType;
+
+
 
 public class BodyNode extends NodeBase<BodyNode> {
     private ArrayList<BodyStatementNodeBase> statements;
@@ -16,9 +19,19 @@ public class BodyNode extends NodeBase<BodyNode> {
     public static BodyNode parseNode(TokenStack tokens) {
         tokens.pushStack();
 
-        
+        BodyNode body = new BodyNode();
+        BodyStatementNodeBase statement;
+
+        while (tokens.peekToken().getTokenType() != TokenType.R_BRACE) {
+            statement = BodyStatementNodeBase.parseNode(tokens);
+            if (statement == null) {
+                tokens.popStack(true);
+                return null;
+            }
+            body.statements.add(statement);
+        }
 
         tokens.popStack(false);
-        return null;
+        return body;
     }
 }
