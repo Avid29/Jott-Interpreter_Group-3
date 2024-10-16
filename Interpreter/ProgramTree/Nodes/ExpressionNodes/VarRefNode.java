bc734265b42ba45854f1b423b79cdacdce3c6cce
amenkes319 <amenkes319@gmail.com>
2024-10-16 16:08:55 -0400

    VarRefNode parseNode implemented

--- Interpreter/ProgramTree/Nodes/ExpressionNodes/VarRefNode.java
+++ Interpreter/ProgramTree/Nodes/ExpressionNodes/VarRefNode.java
@@ -1,12 +1,27 @@
 package Interpreter.ProgramTree.Nodes.ExpressionNodes;
 
+import Interpreter.Parsing.TokenStack;
 import Interpreter.ProgramTree.Nodes.ExpressionNodes.Abstract.OperandNodeBase;
 import provided.Token;
+import provided.TokenType;
 
 public class VarRefNode extends OperandNodeBase {
     private Token id;
 
     public VarRefNode(Token id) {
         this.id = id;
+    }
+
+    public static VarRefNode parseNode(TokenStack tokens){
+        tokens.pushStack();
+
+        var next = tokens.popToken();
+        if (next.getTokenType() != TokenType.ID) {
+            tokens.popStack(true);
+            return null;
+        }
+
+        tokens.popStack(false);
+        return new VarRefNode(next);
     }
 }
